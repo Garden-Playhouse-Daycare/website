e8066e454b0dc2a2bdd9c2d28f31d0ceaed22e08
Manas Nagelia <84251015+Manas-Nagelia@users.noreply.github.com>
2022-11-02 20:41:32 -0400

    Added google maps to the website

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,25 @@ import Head from "next/head";
 import { HeroText } from "../components/HeroSection/HeroText";
 import Image from "next/image";
 import MainImg from "../public/MainImg.webp";
-import { Title, createStyles, MantineTheme } from "@mantine/core";
+import {
+  Title,
+  createStyles,
+  MantineTheme,
+  Container,
+  Center,
+  useMantineTheme
+} from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 import { HeaderResponsive } from "../components/HeaderResponsive";
 import { ArticlesCardsGrid } from "../components/ArticleCardsGrid";
 import { ReviewsGrid } from "../components/Testimonials/ReviewsGrid";
 import { AboutPage } from "../components/About/AboutPage";
 import { Gallery } from "../components/Gallery";
+import GoogleMapReact from "google-map-react";
 
 export default function Home() {
-  const mobileMatch = useMediaQuery("(max-width: 565px)");
+  const theme = useMantineTheme();
+  const mobileMatch = useMediaQuery(`(max-width: ${theme.breakpoints.sm}px)`);
 
   return (
     <>
@@ -67,6 +76,20 @@ export default function Home() {
         Gallery
       </Title>
       <Gallery />
+      <Title mt={90} mb="xl" align="center" weight={800}>
+        Contact Us
+      </Title>
+      <Center mx="xl">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3162.6875863246482!2d-121.97052370000002!3d37.5624237!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fc08b2b3a4f33%3A0x64205c0be4fdf63c!2sGarden%20Playhouse%20Daycare!5e0!3m2!1sen!2sus!4v1667435303590!5m2!1sen!2sus"
+          width={!mobileMatch ? "80%" : "100%"}
+          height={!mobileMatch ? 600 : 400}
+          style={{ border: 0, borderRadius: "2%", display: "block" }}
+          allowFullScreen={undefined}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </Center>
     </>
   );
 }
